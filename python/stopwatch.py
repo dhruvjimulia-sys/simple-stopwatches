23ad934768da9df6c2c68658d1bcbaa12fc9c027
Dhruv Jimulia <dj321@ic.ac.uk>
2022-01-14 19:28:55 +0000

    Added clear_line() function in Python implementation

--- python/stopwatch.py
+++ python/stopwatch.py
@@ -8,11 +8,14 @@ def convert_secs(sec):
     mins = mins % 60
     return (int(hours), int(mins), int(sec))
 
+def clear_line():
+    sys.stdout.write("\033[F")
+
 if __name__ == '__main__':
     time_lapsed = 0
     while True:
         (hours, mins, sec) = convert_secs(time_lapsed)
         print(f"Time lapsed = {hours:02}:{mins:02}:{sec:02}")
-        sys.stdout.write("\033[F")
+        clear_line()
         time.sleep(1)
         time_lapsed = time_lapsed + 1
